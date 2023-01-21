ea9a20d254dd7011ee9be15fff2a689a0e80747a
Mayankd21 <110493499+Mayank-Digarse@users.noreply.github.com>
2023-01-21 23:04:57 +0530

    Create Pattern 21.cpp

---
+++ Pattern 21/Pattern 21.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    void printSquare(int n) {
+        // code here
+        for(int i =0 ;i<n; i++){
+            for(int j= 0; j<n; j++){
+                if(i == 0 || j == 0 || i == n-1 || j == n-1){
+                    cout<<"*";
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            cout<<endl;
+        }
+    }
+};
