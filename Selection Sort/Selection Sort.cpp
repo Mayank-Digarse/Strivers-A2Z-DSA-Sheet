d0e0d92f7a0cc8bd24d658ec5ffa56cb85cb57f1
Mayankd21 <110493499+Mayank-Digarse@users.noreply.github.com>
2023-01-26 21:45:56 +0530

    Create Selection Sort.cpp

---
+++ Selection Sort/Selection Sort.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+    int select(int arr[], int i)
+    {
+        // code here such that selectionSort() sorts arr[]
+       
+        return 0;
+    }
+     
+    void selectionSort(int arr[], int n)
+    {
+       //code here
+       for(int i = 0; i<n-1; i++){
+           int min = i;
+           for(int j = i+1; j<n; j++){
+               if(arr[min]>arr[j]){
+                   min = j;
+               }
+           }
+           swap(arr[min] , arr[i]);
+       }
+       
+    }
+};
