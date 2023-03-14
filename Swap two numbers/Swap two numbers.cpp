f67243b060fdd7551a9a0e70af846741f3f49b2c
Mayankd21 <110493499+Mayank-Digarse@users.noreply.github.com>
2023-03-14 23:48:31 +0530

    Create Swap two numbers.cpp

---
+++ Swap two numbers/Swap two numbers.cpp
@@ -0,0 +1,12 @@
+
+class Solution{   
+public:
+    pair<int, int> get(int a, int b){
+        //complete the function here
+    
+         a = a^b;
+         b = a^b;
+         a = a^b;
+         return {a,b};
+    }
+};
