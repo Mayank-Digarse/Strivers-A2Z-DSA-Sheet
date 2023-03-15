664c72292bad2cf897a8117c1a2e06521527034b
Mayankd21 <110493499+Mayank-Digarse@users.noreply.github.com>
2023-03-16 00:31:00 +0530

    Create Power of 2.cpp

---
+++ Power of 2/Power of 2.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<0) return false;
+        if(n == 0) return false;
+        if(n&(n-1)) return false;
+        else return true;
+    }
+};
