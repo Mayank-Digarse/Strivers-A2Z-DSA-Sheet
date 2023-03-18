be7019f8d800f00a090a0a7dc85299315ccd5074
Mayankd21 <110493499+Mayank-Digarse@users.noreply.github.com>
2023-03-18 14:38:10 +0530

    Create Stack using two queues.cpp

---
+++ Stack using two queues/Stack using two queues.cpp
@@ -0,0 +1,36 @@
+/* The structure of the class is
+class QueueStack{
+private:
+    queue<int> q1;
+    queue<int> q2;
+public:
+    void push(int);
+    int pop();
+};
+ */
+
+//Function to push an element into stack using two queues.
+void QueueStack :: push(int x)
+{
+        // Your Code
+        
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        q1.push(x);
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+}
+
+//Function to pop an element from stack using two queues. 
+int QueueStack :: pop()
+{
+        // Your Code   
+        if(q1.size() == 0) return -1;
+        int val = q1.front();
+        q1.pop();
+        return val;
+}
